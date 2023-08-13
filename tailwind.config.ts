a76926099385439e30adc976735ee08e26f0d53b
Romina Manzano <rominam@nimble.la>
2023-08-13 17:00:36 -0300

    ADD: style guide

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,18 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        primary: {
+          500: 'hsl(236, 72%, 79%)',
+          600: 'hsl(237, 63%, 64%)',
+        },
+        neutral: {
+          200: 'hsl(240, 78%, 98%)',
+          400: 'hsl(234, 14%, 74%)',
+          500: 'hsl(233, 13%, 49%)',
+          600: 'hsl(232, 13%, 33%)',
+        }
+      },
     },
   },
   plugins: [],
